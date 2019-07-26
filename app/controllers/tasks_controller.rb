4d8f56e9fb7c0ec172c7cec0d70b662ff9b4646b
taro.kirameki <ec2-user@ip-172-31-43-68.us-east-2.compute.internal>
2019-07-26 04:24:16 +0000

    kadai2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   def index
-    #@tasks=Task.all
+     #@tasks=Task.all
     
     @tasks = current_user.tasks
   end
